338720764f980ac82645ce1ab7015364a225046b
anupal <anupal19-379@aec.ac.in>
2023-10-07 19:21:46 +0530

    Add multiple pages for each topic.

--- PDF_Generator/main.py
+++ PDF_Generator/main.py
@@ -11,4 +11,7 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, align="L", ln=1, txt=row["Topic"])
     pdf.line(10, 21, 200, 21)
 
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
 pdf.output("Notes.pdf")
